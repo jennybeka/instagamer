948ef942d354de8d30975b2b33f347ae4a7365dd
jennybeka <jennybekadutra@gmail.com>
2020-03-14 21:14:21 +0100

    teste heroku 2

--- src/core/auth/token.middleware.ts
+++ src/core/auth/token.middleware.ts
@@ -8,6 +8,7 @@ export default class TokenMiddleware {
         const allowedPaths = [
             '/',
             '/instagamer/login',
+            '/login.html',
             '/instagamer/register',
         ];
 
